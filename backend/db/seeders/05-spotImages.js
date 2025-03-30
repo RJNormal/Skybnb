bd2d853f65e34df180ecc9a904f307254a99786d
.Rashid Jones <artemys@DESKTOP-D1UFVEN.localdomain>
2025-03-30 19:13:26 -0400

    Fixing Review

--- backend/db/seeders/05-spotImages.js
+++ backend/db/seeders/05-spotImages.js
@@ -11,19 +11,19 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await SpotImage.bulkCreate([
     {
-        url:  "image url",
+        url:  "https://img.freepik.com/premium-photo/surrealistic-image-notary-public-s-office-floating-sky-surrounded-by-fluffy-clouds_891336-47882.jpg",
         preview: true,
         spotId: 1,
         userId:1
     },
     {
-        url:  "image url",
+        url:  "https://img.100r.systems/img/4324132b1f939b0a56173237590e9a11.jpg",
         preview: true,
         spotId: 2,
         userId: 2
     },
     {
-      url:  "image url",
+      url:  "https://img.freepik.com/premium-photo/surreal-floating-island-with-house-top-island-is-covered-green-grass-trees-house-is-made-wood-has-stone-chimney_14117-480617.jpg",
       preview: true,
       spotId: 3,
       userId: 3
